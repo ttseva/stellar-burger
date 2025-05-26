6fcf4b175c30377ff09ced88e194e7ba3fb542f2
Anastasia <annihilcax@gmail.com>
2025-05-27 01:47:41 +0300

    ADD: modal test

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,9 +1,9 @@
-import { defineConfig } from "cypress";
+import { defineConfig } from 'cypress';
 
 export default defineConfig({
   env: {
-    BURGER_API_URL: 'https://norma.nomoreparties.space/api'
-  },
+    BURGER_API_URL: 'https://norma.nomoreparties.space/api',
+ },
   e2e: {
     baseUrl: 'http://localhost:4000',
     setupNodeEvents(on, config) {
--- cypress/e2e/constructor.cy.ts
+++ cypress/e2e/constructor.cy.ts
@@ -1,35 +1,55 @@
+import { deleteCookie, setCookie } from '../../src/utils/cookie';
 
+const API_URL = Cypress.env('BURGER_API_URL');
+const REFRESH_TOKEN = 'test_refreshToken';
+const ACCESS_TOKEN = 'test_accessToken';
 
 describe('Конструктор', () => {
   beforeEach(() => {
-    cy.visit('/');
-    cy.intercept('GET', '/api/ingredients', { fixture: 'ingredients.json' }).as(
-      'getIngredients'
+    setCookie('accessToken', ACCESS_TOKEN);
+    localStorage.setItem('refreshToken', REFRESH_TOKEN);
+
+    cy.intercept('GET', `${API_URL}/auth/user`, { fixture: 'user.json' }).as(
+      'getUser'
     );
-    cy.wait('@getIngredients');
+    cy.intercept('GET', `${API_URL}/ingredients`, {
+      fixture: 'ingredients.json'
+    }).as('getIngredients');
+
+    cy.visit('/');
+    cy.wait('@getUser');
   });
-
-
-
-
 
   it('should display ingredients', () => {
     cy.get('[data-cy=ingredient]').should('have.length.greaterThan', 0);
   });
 
+  it('модальные окна', () => {
+    cy.get('[data-cy=ingredient]').first().click();
+    cy.get('[data-cy=modal]').should('be.visible');
+    cy.get('[data-cy=ingredient-details-name]').should(
+      'contain',
+      'Краторная булка N-200i'
+    );
+
+    cy.get('[data-cy=modal-close]').click();
+    cy.get('[data-cy=modal]').should('not.exist');
+
+    cy.get('[data-cy=ingredient]').first().click();
+    cy.get('[data-cy=modal]').should('be.visible');
+
+    cy.get('[data-cy=modal-overlay]').click({ force: true });
+    cy.get('[data-cy=modal]').should('not.exist');
+  });
+
   it('should add ingredients to the constructor', () => {
-    // Находим булку и добавляем ее
+    // Check that buns are not in the constructor initially
+    cy.get('[data-cy=constructor-bun-top]').should('not.exist');
+    cy.get('[data-cy=constructor-bun-bottom]').should('not.exist');
+
     cy.contains('[data-cy=ingredient]', 'Булка Space').find('button').click();
 
-    // Находим начинку и добавляем ее
-    cy.contains(
-      '[data-cy=ingredient]',
-      'Мясо бессмертных моллюсков Protostomia'
-    )
-      .find('button')
-      .click();
-
-    // Проверяем, что булка появилась в конструкторе (верх и низ)
+    // Check that buns are now in the constructor
     cy.get('[data-cy=constructor-bun-top]').should(
       'contain',
       'Булка Space (верх)'
@@ -39,7 +59,20 @@ describe('Конструктор', () => {
       'Булка Space (низ)'
     );
 
-    // Проверяем, что начинка появилась в конструкторе
+    // Check that the ingredient is not in the constructor initially
+    cy.get('[data-cy=constructor-ingredients]').should(
+      'not.contain',
+      'Мясо бессмертных моллюсков Protostomia'
+    );
+
+    cy.contains(
+      '[data-cy=ingredient]',
+      'Мясо бессмертных моллюсков Protostomia'
+    )
+      .find('button')
+      .click();
+
+    // Check that the ingredient is now in the constructor
     cy.get('[data-cy=constructor-ingredients]').should(
       'contain',
       'Мясо бессмертных моллюсков Protostomia'
